d068ec9c6cf86611700dab96a74282630d938a70
Kaelan Mikowicz <kaelan@uber.com>
2023-01-31 19:10:41 -0800

    fix state iam name

--- terraform/modules/spanner-role/outputs.tf
+++ terraform/modules/spanner-role/outputs.tf
@@ -4,6 +4,6 @@ output "spanner_capacity_manager_iam_role_name" {
 }
 
 output "scaler_state_manager_iam_role_name" {
-  value       = google_project_iam_custom_role.scaler_state_manager.name
+  value       = google_project_iam_custom_role.scaler_state_manager_iam_role.name
   description = "Name of the Scaler State Manager IAM Role"
 }
