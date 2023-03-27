30f8d2332930c12ca7a171caaa1701572b34df94
Benjamin Good <benjamingood@google.com>
2023-03-27 10:05:44 -0600

    Add option for specifying config directly as JSON (#94)
    * Adding more varibles for common values and variable to specify json directly.
    
    * adding an example of passing config as json to the per project example.
    
    * Removing the interpolation from the min and max size varaibles so they are used as raw numbers

--- terraform/modules/scheduler/variables.tf
+++ terraform/modules/scheduler/variables.tf
@@ -45,6 +45,30 @@ variable "target_pubsub_topic" {
   type = string
 }
 
+variable "units" {
+  type        = string
+  default     = "PROCESSING_UNITS"
+  description = "The measure that spanner size units are being specified in either: PROCESSING_UNITS or NODES"
+}
+
+variable "min_size" {
+  type        = number
+  default     = 100
+  description = "Minimum size that the spanner instance can be scaled in to."
+}
+
+variable "max_size" {
+  type        = number
+  default     = 2000
+  description = "Maximum size that the spanner instance can be scaled out to."
+}
+
+variable "scaling_method" {
+  type        = string
+  default     = "LINEAR"
+  description = "Algorithm that should be used to manage the scaling of the spanner instance: STEPWISE, LINEAR, DIRECT"
+}
+
 variable "terraform_spanner_state" {
   description = "If set to true, Terraform will create a Cloud Spanner DB to hold the Autoscaler state."
   type        = bool
@@ -61,4 +85,10 @@ variable "state_spanner_name" {
   type     = string
   nullable = true
   default  = null
+}
+
+variable "json_config" {
+  type        = string
+  default     = ""
+  description = "Base 64 encoded json that is the autoscaler configuration for the Cloud Scheduler payload. Using this allows for setting autoscaler configuration for multiple spanners and parameters that are not directly exposed through variables."
 }
